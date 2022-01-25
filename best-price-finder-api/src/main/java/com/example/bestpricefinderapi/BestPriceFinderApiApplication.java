64f6b0d674cfa3765f875ccefd6d48ab692edb8a
YH.LEE <bnm1128@gmail.com>
2022-01-25 09:51:51 +0900

    inital commit

---
+++ best-price-finder-api/src/main/java/com/example/bestpricefinderapi/BestPriceFinderApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.bestpricefinderapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestPriceFinderApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BestPriceFinderApiApplication.class, args);
+    }
+
+}
---
+++ best-price-finder-api/src/test/java/com/example/bestpricefinderapi/BestPriceFinderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bestpricefinderapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestPriceFinderApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
