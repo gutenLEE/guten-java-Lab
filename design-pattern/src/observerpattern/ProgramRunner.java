dd3f499f5f52baa89ab4b1ed8f0cd86e0ac3b96c
gutenLEE <3bnm1128@naver.com>
2022-03-28 22:12:37 +0900

    add main class to run this observer pattern program

--- design-pattern/src/observerpattern/ProgramRunner.java
+++ design-pattern/src/observerpattern/ProgramRunner.java
@@ -5,6 +5,7 @@ public class ProgramRunner {
         WeatherData weatherData = new WeatherData();
         CurrentConditionsDisplay currentConditionsDisplay = new CurrentConditionsDisplay();
         weatherData.registerObserver(currentConditionsDisplay);
+        weatherData.setMeasurements(0.2f, 0.4f, 0.4f);
 
     }
 }
