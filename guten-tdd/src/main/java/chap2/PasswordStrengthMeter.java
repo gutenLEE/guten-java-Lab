4ea3afeb6b7223f67435730e1dfdc6247fe25121
gutenLEE <3bnm1128@naver.com>
2022-05-18 08:48:44 +0900

    refactoring

--- guten-tdd/src/main/java/chap2/PasswordStrengthMeter.java
+++ guten-tdd/src/main/java/chap2/PasswordStrengthMeter.java
@@ -5,37 +5,23 @@ package chap2;
  * @since 2022-05-17 오전 8:46
  */
 public class PasswordStrengthMeter {
+
     public PasswordStrength meter(String s) {
 
         if ( s == null || s.isEmpty() ) return PasswordStrength.INVALID;
 
-        boolean lengthEnough = s.length() >= 8;
-        boolean containsNum = meetsContainingNumberCriteria(s);
-        boolean containsUpp = meetsContainingUppercaseCritera(s);
+        int meetsCount = 0;
+        if (s.length() >= 8) meetsCount++;
+        if (meetsContainingNumberCriteria(s)) meetsCount++;
+        if (meetsContainingUppercaseCritera(s)) meetsCount++;
 
-        if (lengthEnough && !containsNum && !containsUpp)
-            return PasswordStrength.WEAK;
-        if (!lengthEnough && containsNum && !containsUpp)
-            return PasswordStrength.WEAK;
-        if (!lengthEnough && !containsNum && containsUpp)
-            return PasswordStrength.WEAK;
-
-        if (!lengthEnough) {
-            return PasswordStrength.NORMAL;
-        }
-        if ( !containsNum ) return PasswordStrength.NORMAL;
-        if ( !containsUpp ) return PasswordStrength.NORMAL;
+        // 아래 코드는 1개의 조건을 충족하면 강도가 약함이라는 규칙을 더 잘 표현해줌.
+        if (meetsCount == 1) return PasswordStrength.WEAK;
+        if (meetsCount == 2) return PasswordStrength.NORMAL;
 
         return PasswordStrength.STRONG;
     }
 
-    private boolean meetsContainingUppercaseCritera(String s) {
-        for (char ch : s.toCharArray()) {
-            if (Character.isUpperCase(ch))
-                return true;
-        }
-        return false;
-    }
 
     private boolean meetsContainingNumberCriteria(String s) {
         boolean containsNum = false;
@@ -47,4 +33,14 @@ public class PasswordStrengthMeter {
         }
         return containsNum;
     }
+
+
+    private boolean meetsContainingUppercaseCritera(String s) {
+        for (char ch : s.toCharArray()) {
+            if (Character.isUpperCase(ch))
+                return true;
+        }
+        return false;
+    }
+
 }
