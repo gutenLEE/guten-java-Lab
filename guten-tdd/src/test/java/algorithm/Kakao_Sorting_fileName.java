10a003bc8b6c2b63cc3ea7d980c4047bf0e14362
gutenLEE <3bnm1128@naver.com>
2022-06-06 22:13:30 +0900

    task: 파일명 정렬
    1. 헤드, 넘버, 꼬리 부분으로 자르는 기능부터 검증
    2. 1번 검증 완료 후, 메서드 추출
    3. 정렬 테스트 케이스 통과

--- guten-tdd/src/test/java/algorithm/Kakao_Sorting_fileName.java
+++ guten-tdd/src/test/java/algorithm/Kakao_Sorting_fileName.java
@@ -1,9 +1,12 @@
 package algorithm;
 
 import org.junit.jupiter.api.Test;
-import org.junit.platform.commons.util.StringUtils;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Locale;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -25,13 +28,45 @@ public class Kakao_Sorting_fileName {
         assertSplit("MG01.GIF","MG", "01", ".GIF");
     }
 
-    private void assertSplit(String str, String head, String number, String tail) {
+    @Test
+    void sorting_test() throws Exception {
+        assertSorting(List.of("img12.png", "img10.png", "img02.png", "img1.png", "IMG01.GIF", "img2.JPG"),
+                        List.of("img1.png", "IMG01.GIF", "img02.png", "img2.JPG", "img10.png", "img12.png"));
 
+        assertSorting(List.of("F-5 Freedom Fighter", "B-50 Superfortress", "A-10 Thunderbolt II", "F-14 Tomcat"),
+                        List.of("A-10 Thunderbolt II", "B-50 Superfortress", "F-5 Freedom Fighter", "F-14 Tomcat"));
+    }
+
+    private void assertSorting(List<String> caseList, List<String> expected){
+        List<String[]> arrList = new ArrayList<>();
+        for (String str : caseList) {
+            String[] arr = splitFileName(str);
+            arrList.add(arr);
+        }
+        List<String[]> results = arrList.stream()
+                .sorted(Comparator.<String[], String>comparing(arr -> arr[0].toLowerCase(Locale.ROOT))
+                        .thenComparing(arr2 -> Integer.parseInt(arr2[1])))
+                .collect(Collectors.toList());
+
+        List<String> actual = new ArrayList<>(caseList.size());
+        results.forEach(arr -> {
+            StringBuilder sb = new StringBuilder("");
+            for (String s : arr) {
+                sb.append(s);
+            }
+            actual.add(sb.toString());
+            System.out.println("sb.toString() = " + sb.toString());
+        });
+        assertThat(actual).isEqualTo(expected);
+    }
+
+
+
+    private void assertSplit(String str, String head, String number, String tail) {
         String[] arr = splitFileName(str);
         assertThat(arr[0]).isEqualTo(head);
         assertThat(arr[1]).isEqualTo(number);
         assertThat(arr[2]).isEqualTo(tail);
-
     }
 
     private String[] splitFileName(String str) {
