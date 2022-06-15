8aa348f0e2ebcbaa673c5de622f003bd76004b8d
gutenLEE <3bnm1128@naver.com>
2022-06-16 08:47:05 +0900

    chap 6. - 스파이객체 테스트 통과 - Mockito로 테스트   대역 객체가 기대하는 대로 상호작용했는지 확인하는 것이 모의 객체의 주요 기능이다.

---
+++ guten-tdd/src/test/java/chap6/UserRegisterMockTest.java
@@ -0,0 +1,40 @@
+package chap6;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+/**
+ * @author gutenlee
+ * @since 2022/06/16
+ */
+public class UserRegisterMockTest {
+
+    private UserRegister userRegister;
+    private WeakPasswordChecker mockPasswordChecker =
+            Mockito.mock(WeakPasswordChecker.class);
+    private MemoryUserRepository fakeRepository = new MemoryUserRepository();
+    private EmailNotifier mockEmailNotifier = Mockito.mock(EmailNotifier.class); // 인자로 전달받은 타입의 모의 객체 생성
+
+    @BeforeEach
+    void setUp() {
+        userRegister = new UserRegister(mockPasswordChecker, fakeRepository, mockEmailNotifier);
+    }
+
+    @Test
+    @DisplayName("약한 암호면 가입 실패")
+    void weakPassword() throws Exception {
+
+        BDDMockito.given(mockPasswordChecker.checkPasswordWeak("pw")).willReturn(true);
+
+        assertThrows(WeakPasswordException.class, () -> {
+            userRegister.register("id", "pw", "email");
+        });
+    }
+
+
+}
