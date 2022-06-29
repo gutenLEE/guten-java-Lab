25f870e5ee6dbbac64d37122e4c7277bbc4959cf
gutenLEE <3bnm1128@naver.com>
2022-06-30 08:56:00 +0900

    test

---
+++ spring/src/main/java/com/example/springlab/SpringLabApplication.java
@@ -0,0 +1,14 @@
+package com.example.springlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//sㅈㄷㄹ
+@SpringBootApplication
+public class SpringLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLabApplication.class, args);
+	}
+
+}
