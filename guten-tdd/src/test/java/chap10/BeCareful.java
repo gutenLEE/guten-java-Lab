06f7a03d1661879f585515985a370896ba633358
gutenLEE <3bnm1128@naver.com>
2022-06-30 08:54:25 +0900

    :tada: [spring-lab] starts

--- guten-tdd/src/test/java/chap10/BeCareful.java
+++ guten-tdd/src/test/java/chap10/BeCareful.java
@@ -1,8 +1,20 @@
 package chap10;
 
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+
 /**
  * @author gutenlee
  * @since 2022/06/29
  */
 public class BeCareful {
+
+    @Test
+    void dateFormat() throws Exception {
+
+    }
+
+
 }
