d18cdc8b405131e94a4a252a3b94c28d72ccf4bf
gutenLEE <3bnm1128@naver.com>
2022-09-04 16:11:49 +0900

    feat(상하좌우) - 경계일 경우 예외 처리 추가

--- guten-tdd/src/main/java/algorithm/Move.java
+++ guten-tdd/src/main/java/algorithm/Move.java
@@ -7,20 +7,30 @@ package algorithm;
 public class Move {
     private int x;
     private int y;
+    private int boundary;
 
+    public Move(int boundary) {
+        this.x = 1;
+        this.y = 1;
+        this.boundary = boundary;
+    }
 
     public void goTo(String direction) {
         switch (direction) {
             case "U":
+                if (x == 1)break;
                 x--;
                 break;
             case "D":
+                if (x == boundary) break;
                 x++;
                 break;
             case "R":
+                if (y == boundary) break;
                 y++;
                 break;
             case "L":
+                if (y == 1) break;
                 y--;
                 break;
         }
--- guten-tdd/src/test/java/algorithm/UpDownLeftRight.java
+++ guten-tdd/src/test/java/algorithm/UpDownLeftRight.java
@@ -2,7 +2,10 @@ package algorithm;
 
 import org.assertj.core.util.Arrays;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import javax.swing.*;
 
@@ -24,7 +27,7 @@ public class UpDownLeftRight {
 
     @BeforeEach
     void setUp() {
-        move = new Move();
+        move = new Move(3);
     }
 
     @Test
@@ -33,95 +36,43 @@ public class UpDownLeftRight {
         assertThat(directions("R R R U D D")).contains("R", "R", "R", "U", "D", "D");
         String[] directions = directions("R R R U D D");
 
-        int[] finalPosition = getFinalPosition(directions);
-        assertThat(finalPosition[0]).isEqualTo(3);
-        assertThat(finalPosition[1]).isEqualTo(4);
+        Move move = new Move(5);
+        for (String direction : directions) {
+            move.goTo(direction);
+        }
+        assertThat(move.getX()).isEqualTo(3);
+        assertThat(move.getY()).isEqualTo(4);
     }
-
 
     @Test
     void right() throws Exception {
-        move.goTo("R");
+        for (int i = 1; i <= 4; i++) {
+            move.goTo("R");
+        }
         assertThat(move.getX()).isEqualTo(0);
-        assertThat(move.getY()).isEqualTo(1);
+        assertThat(move.getY()).isEqualTo(3);
     }
 
     @Test
     void left() throws Exception {
-        move.goTo("L");
         assertThat(move.getX()).isEqualTo(0);
-        assertThat(move.getY()).isEqualTo(-1);
+        assertThat(move.getY()).isEqualTo(0);
     }
 
     @Test
     void up() throws Exception {
         move.goTo("U");
-        assertThat(move.getX()).isEqualTo(-1);
+        assertThat(move.getX()).isEqualTo(0);
         assertThat(move.getY()).isEqualTo(0);
     }
 
     @Test
     void down() throws Exception {
-        move.goTo("D");
-        assertThat(move.getX()).isEqualTo(1);
-        assertThat(move.getY()).isEqualTo(0);
-    }
-
-
-
-
-
-
-
-
-
-    private int[] getFinalPosition(String[] directions) {
-        int upBoundary = 0;
-        int downBoundary = directions.length -1;
-        int leftBoundary = 0;
-        int rightBoundary = directions[0].length() -1;
-
-        int startY = 0;
-        int startX = 0;
-
-        int curX = startX;
-
-        int curY = startY;
-        for (int i = 0; i < directions.length; i++) {
-            String direction = directions[i];
-            int curPos = 0;
-            switch (direction) {
-                case "U":
-                    curPos = curX--;
-                    if (curPos > upBoundary) {
-                        curX += curPos;
-                    }
-                    break;
-                case "D":
-                    curPos = curX++;
-                    if (curPos < downBoundary) {
-                        curX += curPos;
-                    }
-                    break;
-                case "R":
-                    curPos = curY++;
-                    if (curPos < rightBoundary){
-                        curY += curPos;
-                    }
-                    break;
-                case "L":
-                    curPos = curY--;
-                    if (curPos > leftBoundary) {
-                        curY += curPos;
-                    }
-                    break;
-            }
-            if (i == directions.length - 1) {
-                return new int[]{curX+1, curY+1};
-            }
+        for (int i = 1; i <= 4; i++) {
+            move.goTo("D");
         }
-
-        return new int[]{-1, -1};
+        assertThat(move.getX()).isEqualTo(3);
+        assertThat(move.getY()).isEqualTo(0);
     }
 
     private String[] directions(String str){
