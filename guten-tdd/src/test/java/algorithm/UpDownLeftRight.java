18250e40e93e25a2b8c41939fc2f6d103a236a10
gutenLEE <3bnm1128@naver.com>
2022-09-04 16:13:49 +0900

    :memo: 상하좌우 - 9월 1week_01

--- guten-tdd/src/test/java/algorithm/UpDownLeftRight.java
+++ guten-tdd/src/test/java/algorithm/UpDownLeftRight.java
@@ -1,17 +1,8 @@
 package algorithm;
 
-import org.assertj.core.util.Arrays;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
-
-import javax.swing.*;
-
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.withPrecision;
-import static org.assertj.core.api.InstanceOfAssertFactories.atomicStampedReference;
 
 /**
  * @author gutenlee
