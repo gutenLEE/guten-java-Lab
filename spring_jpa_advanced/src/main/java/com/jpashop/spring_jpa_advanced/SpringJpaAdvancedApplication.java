6ad81db8ff59fdbe21947ebaa5c29c3fa2c9338b
gutenLEE <3bnm1128@naver.com>
2022-09-13 22:42:31 +0900

    :tada: jpa study start

---
+++ spring_jpa_advanced/src/main/java/com/jpashop/spring_jpa_advanced/SpringJpaAdvancedApplication.java
@@ -0,0 +1,13 @@
+package com.jpashop.spring_jpa_advanced;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaAdvancedApplication {
+    //
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJpaAdvancedApplication.class, args);
+    }
+
+}
---
+++ spring_jpa_advanced/src/test/java/com/jpashop/spring_jpa_advanced/SpringJpaAdvancedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpashop.spring_jpa_advanced;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaAdvancedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
