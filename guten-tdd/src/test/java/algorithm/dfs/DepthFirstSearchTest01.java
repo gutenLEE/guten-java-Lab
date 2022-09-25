6e63185ed7a22f19b933fba619868b427dc2dae4
gutenLEE <3bnm1128@naver.com>
2022-09-25 21:30:11 +0900

    fix: 이진트리순회, 전위순회 출력 - 9월 4week_03

---
+++ guten-tdd/src/test/java/algorithm/dfs/DepthFirstSearchTest01.java
@@ -0,0 +1,43 @@
+package algorithm.dfs;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author gutenlee
+ * @since 2022/09/25
+ */
+public class DepthFirstSearchTest01 {
+
+    @Test
+    void test() throws Exception {
+        Node root = new Node(1);
+        root.lt = new Node(2);
+        root.rt = new Node(3);
+        root.lt.lt = new Node(4);
+        root.lt.rt = new Node(5);
+        root.rt.lt = new Node(6);
+        root.rt.rt = new Node(7);
+        dfs(root);
+    }
+
+    public void dfs(Node root){
+        if (root == null) return;
+        else {
+            System.out.print(root.data + " ");
+            dfs(root.lt);
+            dfs(root.rt);
+        }
+    }
+
+
+}
+
+class Node {
+    int data;
+    Node lt, rt;
+
+    public Node(int data) {
+        this.data = data;
+        lt=rt=null;
+    }
+}
