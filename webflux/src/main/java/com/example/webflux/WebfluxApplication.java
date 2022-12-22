78f410a58ff0e20f05b0b6f418b09cf5455ac8cf
gutenLEE <3bnm1128@naver.com>
2022-12-22 22:57:43 +0900

    set classes to test

--- webflux/src/main/java/com/example/webflux/WebfluxApplication.java
+++ webflux/src/main/java/com/example/webflux/WebfluxApplication.java
@@ -2,8 +2,12 @@ package com.example.webflux;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+@EnableMongoAuditing
 @SpringBootApplication
+@EnableMongoRepositories(basePackages = {"com.example.webflux.cache"})
 public class WebfluxApplication {
 
     public static void main(String[] args) {
